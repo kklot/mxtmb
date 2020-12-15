ba10732200a9b5ef0e281606dbf794e7c7b3892a
Kinh Nguyen <fieeuvaan@gmail.com>
2020-12-15 10:52:29 +0100

    catch not continuous ages in other-report

--- R/dodo.R
+++ R/dodo.R
@@ -35,7 +35,7 @@ else {
 if (!nrow(dt)) return(NULL)
 
 # Random walk
-age_id   <- dt %>% pull(age) %>% unique %>% sort
+age_id   <- dt %>% pull(age) %>% range2seq
 meta$min_age <- min(age_id)
 n_age_id <- length(age_id)
 R_age    <- genR(n_age_id, rw_order, scale=FALSE)
